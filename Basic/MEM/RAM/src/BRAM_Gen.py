1aa60acaae4305490428078e652fa1e52dde8533
MoonGrt <1561145394@qq.com>
2025-06-17 13:50:13 +0800

    refactor: BRAM

--- Basic/MEM/RAM/src/BRAM_Gen.py
+++ Basic/MEM/RAM/src/BRAM_Gen.py
@@ -1,74 +1,78 @@
 import sys
 
-def generate_bram_module(N=4, DP=512):
+def BRAM_DPRAM_Gen(N=4, DP=512, DW=8):
     header = f"""\
 /*
-    // BRAM{N} Parameters
+    // DPBRAM{N} Parameters
     parameter FILE_BASE = "init";
     parameter DP = {DP};
+    parameter DW = {DW};
     parameter N = {N};
     parameter AW = $clog2(DP) - 1;
-    parameter DW = N * 8 - 1;
-    // BRAM{N} Signals
-    wire        en = 0;
-    wire        clka = 0;
-    wire        rsta = 0;
-    wire        wra = 0;
-    wire [AW:0] addra = 0;
-    wire [DW:0] dina = 0;
-    wire [DW:0] douta;
-    wire        clkb = 0;
-    wire        rstb = 0;
-    wire        wrb = 0;
-    wire [AW:0] addrb = 0;
-    wire [DW:0] dinb = 0;
-    wire [DW:0] doutb;
-
-    BRAM{N} #(
+    parameter BDW = N * DW - 1;
+    // DPBRAM{N} Signals
+    reg          en;
+    reg          clka;
+    reg          rsta;
+    reg          wra;
+    reg  [ AW:0] addra;
+    reg  [BDW:0] dina;
+    wire [BDW:0] douta;
+    reg          clkb;
+    reg          rstb;
+    reg          wrb;
+    reg  [ AW:0] addrb;
+    reg  [BDW:0] dinb;
+    wire [BDW:0] doutb;
+    // DPBRAM{N} Instance
+    DPBRAM{N} #(
         .FILE_BASE(FILE_BASE),
         .DP       (DP),
+        .DW       (DW),
         .N        (N),
         .AW       (AW),
-        .DW       (DW)
-    ) BRAM{N} (
-        .en   (en),
+        .BDW      (BDW)
+    ) DPBRAM{N} (
         .clka (clka),
         .rsta (rsta),
+        .cea  (cea),
         .wra  (wra),
         .addra(addra),
         .dina (dina),
+        .douta(douta),
         .clkb (clkb),
         .rstb (rstb),
+        .ceb  (ceb),
         .wrb  (wrb),
         .addrb(addrb),
         .dinb (dinb),
-        .douta(douta),
         .doutb(doutb)
     );
 */
 
-module BRAM{N} #(
+module DPBRAM{N} #(
     parameter FILE_BASE = "init",   // File base name
-    parameter DP = {DP},             // DPRAM depth (number of words)
-    parameter N  = {N},               // Number of DPRAM instances (8-bit each)
+    parameter DP = {DP},             // DPRAM depth
+    parameter DW = {DW},               // DPRAM width
+    parameter N  = {N},               // Number of DPRAM instances (DW-bit each)
     parameter AW = $clog2(DP) - 1,  // Address width (log2(DP))
-    parameter DW = N * 8 - 1        // Data width
+    parameter BDW = N * DW - 1      // Data width
 ) (
-    input wire en,
+    input  wire         clka,
+    input  wire         rsta,
+    input  wire         cea,
+    input  wire         wra,
+    input  wire [ AW:0] addra,
+    input  wire [BDW:0] dina,
+    output wire [BDW:0] douta,
 
-    input  wire        clka,
-    input  wire        rsta,
-    input  wire        wra,
-    input  wire [AW:0] addra,
-    input  wire [DW:0] dina,
-    output wire [DW:0] douta,
-
-    input  wire        clkb,
-    input  wire        rstb,
-    input  wire        wrb,
-    input  wire [AW:0] addrb,
-    input  wire [DW:0] dinb,
-    output wire [DW:0] doutb
+    input  wire         clkb,
+    input  wire         rstb,
+    input  wire         ceb,
+    input  wire         wrb,
+    input  wire [ AW:0] addrb,
+    input  wire [BDW:0] dinb,
+    output wire [BDW:0] doutb
 );
 """
 
@@ -79,9 +83,91 @@ module BRAM{N} #(
 """
     for i in range(N):
         insts += f"""\
-    DPRAM #(.INIT_FILE(INIT_FILE{i}), .DP(DP)) DPRAM{i} (.en(en),
-        .clka(clka), .rsta(rsta), .wra(wra), .addra(addra), .dina(dina[{(i+1)*8-1}:{i*8}]), .douta(douta[{(i+1)*8-1}:{i*8}]),
-        .clkb(clkb), .rstb(rstb), .wrb(wrb), .addrb(addrb), .dinb(dinb[{(i+1)*8-1}:{i*8}]), .doutb(doutb[{(i+1)*8-1}:{i*8}]));
+    DPRAM #(.INIT_FILE(INIT_FILE{i}), .DP(DP), .DW(DW)) DPRAM{i} (
+        .clka(clka), .rsta(rsta), .cea(cea), .wra(wra), .addra(addra), .dina(dina[{(i+1)*8-1}:{i*8}]), .douta(douta[{(i+1)*8-1}:{i*8}]),
+        .clkb(clkb), .rstb(rstb), .ceb(ceb), .wrb(wrb), .addrb(addrb), .dinb(dinb[{(i+1)*8-1}:{i*8}]), .doutb(doutb[{(i+1)*8-1}:{i*8}]));
+"""
+
+    footer = "\nendmodule\n"
+
+    return header + insts + footer
+
+def BRAM_SDPRAM_Gen(N=4, DP=512, DW=8):
+    header = f"""\
+/*
+    // SDPBRAM{N} Parameters
+    parameter T = 10;
+    parameter FILE_BASE = "init";
+    parameter DP = {DP};
+    parameter DW = {DW};
+    parameter N = {N};
+    parameter AW = $clog2(DP) - 1;
+    parameter BDW = N * DW - 1;
+    // SDPBRAM{N} Inputs
+    reg          clka;
+    reg          rsta;
+    reg          cea;
+    reg  [ AW:0] addra;
+    reg  [BDW:0] dina;
+    reg          clkb;
+    reg          rstb;
+    reg          ceb;
+    reg  [ AW:0] addrb;
+    wire [BDW:0] doutb;
+    // SDPBRAM{N} Instance
+    SDPBRAM{N} #(
+        .FILE_BASE(FILE_BASE),
+        .DP       (DP),
+        .DW       (DW),
+        .N        (N),
+        .AW       (AW),
+        .BDW      (BDW)
+    ) SDPBRAM{N} (
+        .clka (clka),
+        .rsta (rsta),
+        .cea  (cea),
+        .addra(addra),
+        .dina (dina),
+        .clkb (clkb),
+        .rstb (rstb),
+        .ceb  (ceb),
+        .addrb(addrb),
+        .doutb(doutb)
+    );
+*/
+
+module SDPBRAM{N} #(
+    parameter FILE_BASE = "init",   // File base name
+    parameter DP = {DP},             // DPRAM depth
+    parameter DW = {DW},               // DPRAM width
+    parameter N  = {N},               // Number of DPRAM instances (DW-bit each)
+    parameter AW = $clog2(DP) - 1,  // Address width (log2(DP))
+    parameter BDW = N * DW - 1      // Data width
+) (
+    input  wire         clka,
+    input  wire         rsta,
+    input  wire         cea,
+    input  wire [ AW:0] addra,
+    input  wire [BDW:0] dina,
+
+    input  wire         clkb,
+    input  wire         rstb,
+    input  wire         ceb,
+    input  wire [ AW:0] addrb,
+    output wire [BDW:0] doutb
+);
+"""
+
+    insts = "\n"
+    for i in range(N):
+        insts += f"""\
+    localparam INIT_FILE{i} = {{FILE_BASE, "{i}.hex"}};
+"""
+    for i in range(N):
+        insts += f"""\
+    SDPRAM #(.INIT_FILE(INIT_FILE{i}), .DP(DP), .DW(DW)) SDPRAM{i} (
+        .clka (clka), .rsta (rsta), .cea  (cea), .addra(addra), .dina(dina[{(i+1)*8-1}:{i*8}]),
+        .clkb (clkb), .rstb (rstb), .ceb  (ceb), .addrb(addrb), .doutb(doutb[{(i+1)*8-1}:{i*8}]));
 """
 
     footer = "\nendmodule\n"
@@ -93,6 +179,7 @@ def main():
     # 默认参数
     N = 4
     DP = 512
+    DW = 8
 
     # 读取命令行参数
     if len(sys.argv) > 1:
@@ -105,15 +192,25 @@ def main():
             DP = int(sys.argv[2])
         except:
             print("Invalid dp value, using default 512")
+    if len(sys.argv) > 3:
+        try:
+            DW = int(sys.argv[3])
+        except:
+            print("Invalid dw value, using default 8")
 
-    verilog_code = generate_bram_module(N, DP)
-
-    filename = f"BRAM{N}.v"
+    # BRAM_DPRAM_Gen
+    verilog_code = BRAM_DPRAM_Gen(N, DP, DW)
+    filename = f"DPBRAM{N}.v"
     with open(filename, "w") as f:
         f.write(verilog_code)
+    print(f"Generated {filename} with N={N}, DP={DP}, DW={DW}")
 
-    print(f"Generated {filename} with N={N}, DP={DP}")
-
+    # BRAM_SDPRAM_Gen
+    verilog_code = BRAM_SDPRAM_Gen(N, DP, DW)
+    filename = f"SDPBRAM{N}.v"
+    with open(filename, "w") as f:
+        f.write(verilog_code)
+    print(f"Generated {filename} with N={N}, DP={DP}, DW={DW}")
 
 if __name__ == "__main__":
     main()
--- Basic/MEM/RAM/src/SDPRAM.v
+++ Basic/MEM/RAM/src/SDPRAM.v
@@ -1,68 +1,57 @@
 /*
-    // DPRAM Parameters
+    // SDPRAM Parameters
+    parameter T = 10;
     parameter INIT_FILE = "init.hex";
     parameter DP = 512;
     parameter DW = 8;
     parameter AW = $clog2(DP) - 1;
-    // DPRAM Signals
+    // SDPRAM Inputs
     reg           clka;
     reg           rsta;
     reg           cea;
-    reg           wra;
     reg  [AW-1:0] addra;
     reg  [DW-1:0] dina;
-    wire [DW-1:0] douta;
     reg           clkb;
     reg           rstb;
     reg           ceb;
-    reg           wrb;
     reg  [AW-1:0] addrb;
-    reg  [DW-1:0] dinb;
     wire [DW-1:0] doutb;
-    // DPRAM Instance
-    DPRAM #(
+    // SDPRAM Instance
+    SDPRAM #(
         .INIT_FILE(INIT_FILE),
         .DP       (DP),
         .DW       (DW),
         .AW       (AW)
-    ) DPRAM (
+    ) SDPRAM (
         .clka (clka),
         .rsta (rsta),
         .cea  (cea),
-        .wra  (wra),
         .addra(addra),
         .dina (dina),
-        .douta(douta),
         .clkb (clkb),
         .rstb (rstb),
         .ceb  (ceb),
-        .wrb  (wrb),
         .addrb(addrb),
-        .dinb (dinb),
         .doutb(doutb)
     );
 */
 
-module DPRAM #(
+module SDPRAM #(
     parameter INIT_FILE = "init.hex",
     parameter DP = 512,
     parameter DW = 8,
     parameter AW = $clog2(DP)
 ) (
-    input  wire          clka,
-    input  wire          rsta,
-    input  wire          cea,
-    input  wire          wra,
-    input  wire [AW-1:0] addra,
-    input  wire [DW-1:0] dina,
-    output reg  [DW-1:0] douta,
+    input wire          clka,
+    input wire          rsta,
+    input wire          cea,
+    input wire [AW-1:0] addra,
+    input wire [DW-1:0] dina,
 
     input  wire          clkb,
     input  wire          rstb,
     input  wire          ceb,
-    input  wire          wrb,
     input  wire [AW-1:0] addrb,
-    input  wire [DW-1:0] dinb,
     output reg  [DW-1:0] doutb
 );
 
@@ -85,16 +74,14 @@ module DPRAM #(
     // Port A logic
     wire CLKA = cea ? clka : 1'b0;
     always @(posedge CLKA) begin
-        if (rsta) douta <= {DW{1'b0}};
-        else if (wra) ram[addra] <= dina;
-        else douta <= ram[addra];
+        if (rsta) ram[addra] <= {DW{1'b0}};
+        else ram[addra] <= dina;
     end
 
     // Port B logic
     wire CLKB = ceb ? clkb : 1'b0;
     always @(posedge CLKB) begin
         if (rstb) doutb <= {DW{1'b0}};
-        else if (wrb) ram[addrb] <= dinb;
         else doutb <= ram[addrb];
     end
 
